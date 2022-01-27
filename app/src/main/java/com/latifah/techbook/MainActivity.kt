49d3d726c749643cc654eaba36b42cb47559c875
Robin Blake <robindeanblake@yahoo.com>
2022-01-27 07:31:40 -0500

    Added Test Comment to Main Activity

--- app/src/main/java/com/latifah/techbook/MainActivity.kt
+++ app/src/main/java/com/latifah/techbook/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //a test comment to see if Robin's pushes/branching is all good
     }
 }
