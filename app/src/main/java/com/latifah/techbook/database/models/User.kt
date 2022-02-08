2d2e6e591e49c2edc1f68cb72d44d31eed527730
latifahpresident <latifah.pres@gmail.com>
2022-02-08 14:15:40 -0600

    created user data model

---
+++ app/src/main/java/com/latifah/techbook/database/models/User.kt
@@ -0,0 +1,44 @@
+package com.latifah.techbook.database.models
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class User(
+    val uid : String = "",
+    val firstName: String = "",
+    val lastName : String = "",
+    val email : String = "",
+    val userName : String = "",
+//    val image: String = ""
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(uid)
+        parcel.writeString(firstName)
+        parcel.writeString(lastName)
+        parcel.writeString(email)
+        parcel.writeString(userName)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
