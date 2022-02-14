87b02094feadcee554344791a3be579813ea9e17
ana <pichardo.ana.b@gmail.com>
2022-02-14 00:22:07 -0500

    clean up recycler view TechEventAdapter.kt,postAdapter and created a contact_item_layout.xml, ContactAdapter.kt, ContactFragment.kt

--- app/src/main/java/com/latifah/techbook/ui/fragments/ContactFragment.kt
+++ app/src/main/java/com/latifah/techbook/ui/fragments/ContactFragment.kt
@@ -6,55 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.latifah.techbook.R
+import com.latifah.techbook.database.models.ContactItem
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ContactFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ContactFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false)
+        return inflater.inflate(R.layout.contact_item_layout, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContactFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ContactFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val contactItem = dummieData(20)
+    }
+
+    private fun dummieData(size:Int):List<ContactItem> {
+        val list = ArrayList<ContactItem>()
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_person_icon
+                1 -> R.drawable.ic_contact_person
+                else -> R.drawable.ic_navigate_next
             }
+            val item = ContactItem(drawable, "Item $i", "Line 2")
+            list += item
+        }
+        return list
     }
 }
