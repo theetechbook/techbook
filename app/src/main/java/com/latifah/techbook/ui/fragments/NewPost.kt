52b48d7e5d7a8c47b883ff1962ee8238ef26ce15
latifahpresident <latifah.pres@gmail.com>
2022-03-17 13:56:34 -0400

    Gif upload working. Created function to add gif to image view using Glide

--- app/src/main/java/com/latifah/techbook/ui/fragments/NewPost.kt
+++ app/src/main/java/com/latifah/techbook/ui/fragments/NewPost.kt
@@ -1,13 +1,17 @@
 package com.latifah.techbook.ui.fragments
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
 import com.latifah.techbook.R
 import com.latifah.techbook.databinding.FragmentNewPostBinding
 
@@ -17,6 +21,7 @@ class NewPost : BaseFragment() {
     override var bottomNavigationViewVisibility = View.VISIBLE
     private var _binding: FragmentNewPostBinding? = null
     private val binding get() = _binding!!
+    private lateinit var imageUri : Uri
 
     companion object {
         val IMAGE_REQUEST_CODE = 1_000;
@@ -44,7 +49,20 @@ class NewPost : BaseFragment() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
 
-    startActivityForResult(intent, IMAGE_REQUEST_CODE)
-}
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Log.d("image uploaded", "${data?.data}")
+            activity?.let {
+                Glide.with(it)
+                    .load(data?.data)
+                    .into(binding.imageViewUploadGif)
+            }
+            imageUri = data?.data!!
+        }
+    }
 
 }
